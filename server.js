e5af5e872d13d340118e6dec0d9c4b79bb26ca4a
Shadowborn <james.starks023@gmail.com>
2019-06-03 14:25:10 -0600

    Get and post from and to database working

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+
+const db = require('./data/accounts-model');
 
 // your code here
+
+
+server.get('/', async (req, res) => {
+    try {
+      const budget = await db.find(req.query.id);
+      res.status(200).json({budget});
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Cannot retrieve the accounts' });
+    }
+  });
+
+  server.post('/', async (req, res) => {
+    try {
+        const projects = await db.add(req.body);
+        res.status(201).json(projects);
+      } catch (error) {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+          message: 'Error adding the post',
+        });
+      }
+});
+
+  // server.get('/', (req, res) => {
+  //   res.send(`<h2>Let's write some middleware!</h2>`)
+  // });
 
 module.exports = server;
